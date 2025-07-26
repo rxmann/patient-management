bcb25dfc8e047ee8b3672d3c4b15a51655806f7f
rxmann <k.romann413@gmail.com>
2025-07-26 19:21:19 +0545

    fix(): added billing service with grpc server

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,29 @@
+package com.pm.billingservice.grpc;
+
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+//Registers the service with the gRPC server (from Spring Boot gRPC starter)
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(BillingRequest billingRequest, StreamObserver<BillingResponse> responseObserver) {
+        log.info("CreateBillingAccount request received {} ", billingRequest.toString());
+        // business logic - save to db, perform calculation etc
+        BillingResponse billingResponse = BillingResponse.newBuilder().setAccountId("XAE451").setStatus("SUCCESS").build();
+//        Sends a response
+        responseObserver.onNext(billingResponse);
+//        Closes the RPC session
+        responseObserver.onCompleted();
+    }
+
+}
--- patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
+++ patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.pm.patientservice.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
